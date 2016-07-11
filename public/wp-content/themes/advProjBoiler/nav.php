f25c7cc5e7f8a1a4192bf31ae77f67a39df983a6
Robert James Algeo <rjalgeo@gmail.com>
2016-07-11 10:53:48 -0500

    SCSS and Template Edits
    Added SCSS Files for about and work, added header.php, added nav.php

---
+++ public/wp-content/themes/advProjBoiler/nav.php
@@ -0,0 +1,12 @@
+<header>
+   <!--
+      <img src="img.file"  />
+      <h1 class="visuallyhidden" >Noel Nissen - Drumming</h1>
+   -->
+   <h1><?php bloginfo( "name"); ?> | <?php bloginfo( "description" ); ?></h1>
+   <nav>
+     <ul>
+       <?php wp_nav_menu( array( "menu" => "Main Nav")); ?>
+     </ul>
+   </nav>
+ </header>
